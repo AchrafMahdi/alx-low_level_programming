fff026580e7bfc5bbc5c7eb3c8339164377ade86
achraf <achrafmahdi00@gmail.com>
2023-10-09 16:28:43 -0700

    Done with the 6th task :)

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		printf("%d\n", num);
+	}
+	return (0);
+}
