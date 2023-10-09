47c49ac4bdf9fa884d11aa7acccbcc07d641ac0e
achraf <achrafmahdi00@gmail.com>
2023-10-09 11:42:25 -0700

    Finished the 5th task :)

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-int main(void){
+
+/**
+ * main - the main function
+ * Return: Always 0
+*/
+
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
