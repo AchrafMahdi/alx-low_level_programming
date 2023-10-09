192af09d75e2b8232b36d45cb5da24ebd6d817b8
achraf <achrafmahdi00@gmail.com>
2023-10-09 16:10:51 -0700

    DOne with the 4th task :)

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	char letter = 'a';
+	char LETTER = 'A';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	while (LETTER <= 'Z')
+	{
+		putchar(LETTER);
+		LETTER++;
+	}
+	putchar("\n");
+	return (0);
+}
