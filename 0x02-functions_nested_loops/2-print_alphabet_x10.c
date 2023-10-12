7879a02dd3734bd1e41c2129f5c505e2156c6b51
achraf <achrafmahdi00@gmail.com>
2023-10-11 17:40:37 -0700

    DONE...?

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,9 +7,11 @@
 
 void print_alphabet_x10(void)
 {
-	for (int i = 0; i <= 10; i++)
+	int i,letter;
+
+	for (i = 0; i <= 10; i++)
 	{
-		for (char letter = 'a'; letter <= 'z'; letter++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
 		}
