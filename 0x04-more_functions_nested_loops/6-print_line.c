42e05b32d4ca4c383e7af00e51cb43f208be1d65
achraf <achrafmahdi00@gmail.com>
2023-10-13 16:22:59 -0700

    Done with 7... :)

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - prints straight line
+ * @n: provided input
+*/
+
+
+void print_line(int n)
+{
+	int line;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (line = 1; line <= n; line++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+
+}
