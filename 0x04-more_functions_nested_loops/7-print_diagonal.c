4f3fc66d4be80e9e8178263d97e5a1d7c7414d76
Achraf <achraf@example.com>
2024-06-29 02:38:15 +0100

    Hopefully #7 is fixed

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,9 +16,9 @@ void print_diagonal(int n)
 	}
 	else
 	{
-		for (p = 1; p <= n; p++)
+		for (p = 0; p < n; p++)
 		{
-			for (s = 0; s <= p; s++)
+			for (s = 0; s < p; s++)
 			{
 				_putchar(' ');
 			}
