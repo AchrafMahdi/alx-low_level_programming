4920f14c29e43d3d037a5453e07ba4ac391fe25b
Achraf <achraf@example.com>
2024-07-02 01:48:09 +0100

    #3 DONE

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * _puts - function that prints a string, followed by a new line
+ * @str: pointer to a string 
+ */
+void _puts(char *str)
+{
+int length = 0;
+
+while (*str != '\0')
+{
+    putchar(*str);
+    str++;
+}
+putchar('\n');
+return (length);
+}
