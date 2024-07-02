6b0c66f92b02d5e77c97d9a374c43cb570ba5457
Achraf <achraf@example.com>
2024-07-02 03:37:08 +0100

    #4 DONE

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,18 +7,16 @@
 void print_rev(char *s)
 {
 int len = 0;
-
 while (*s != '\0')
 {
-    len++;
-    s++;
+len++;
+s++;
 }
-
 while (len > 0)
 {
-    s--;
-    putchar(*s);
-    len--;
+s--;
+putchar(*s);
+len--;
 }
 putchar('\n');
 }
