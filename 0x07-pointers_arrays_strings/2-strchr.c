380886936dc6deeefa7b10cfc38702078e509621
Achraf <achraf@example.com>
2024-07-09 00:31:45 +0100

    #2 DONE ?

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,16 +8,21 @@
 char *_strchr(char *s, char c)
 {
 unsigned int i;
+
 for (i = 0; s[i] != '\0'; i++)
 {
+
 if (c == s[i])
 {
 return (s + i);
 }
+
 }
+
 if (c == '\0')
 {
 return (s + i);
 }
+
 return (NULL);
 }
