625f78caec58314dcd4551552441448cfb76f21e
Achraf <achraf@example.com>
2024-07-09 04:31:48 +0100

    #6 chess is <3

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 /**
-* print_chessboard - print chessboard given set 2D array
-* @a: 2D array
-*/
+ * print_chessboard - print chessboard given a 2D array
+ * @a: 2D array
+ */
 void print_chessboard(char (*a)[8])
 {
 unsigned int i;
