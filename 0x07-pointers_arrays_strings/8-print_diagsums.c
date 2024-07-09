074663678d9eeb58f16c8c009216b5f07525d98e
Achraf <achraf@example.com>
2024-07-09 04:27:04 +0100

    #7 is done

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,11 +8,13 @@ void print_diagsums(int *a, int size)
 {
 int s1 = 0;
 int s2 = 0;
-unsigned int i;
+int i;
+
 for (i = 0; i < size; i++)
 {
 s1 += a[i * size + i];
 s2 += a[i * size + (size - i - 1)];
 }
-printf("%i, %i\n", s1, s2);
+
+printf("%d, %d\n", s1, s2);
 }
