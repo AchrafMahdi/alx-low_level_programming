ca6a9614986d046d5cab1fae1095fd9ae46cc583
Achraf <achraf@example.com>
2024-07-09 02:33:01 +0100

    #3 FIXED

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,22 +9,19 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int count = 0;
 int j = 0;
 int i = 0;
-
+unsigned int count = 0;
 while (s[i] != '\0' && s[i] != ' ')
 {
 while (accept[j] != '\0')
 {
-
 if (s[i] == accept[j])
 {
 j++;
 count++;
 }
 }
-
 i++;
 j = 0;
 }
