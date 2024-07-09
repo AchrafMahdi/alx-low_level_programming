1e925a5b318cf1d5eab535d4655e78039bee73b3
Achraf <achraf@example.com>
2024-07-09 04:28:25 +0100

    #6 chess <3

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,11 +7,12 @@ void print_chessboard(char (*a)[8])
 {
 unsigned int i;
 unsigned int j;
+
 for (i = 0; i < 8; i++)
 {
 for (j = 0; j < 8; j++)
 {
-printf("%c",a[i][j]);
+printf("%c", a[i][j]);
 }
 printf("\n");
 }
