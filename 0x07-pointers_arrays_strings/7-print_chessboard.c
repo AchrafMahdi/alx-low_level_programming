c81472de5d7d21405ca6bc605a6f5409c7cb6a9e
Achraf <achraf@example.com>
2024-07-09 03:21:42 +0100

    #7 i love chess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * print_chessboard - function that prints chessboard.
+ *@a: char
+ */
+void print_chessboard(char (*a)[8])
+{
+unsigned int i = 0;
+unsigned int j = 0;
+
+for (;i < 8; i++)
+{
+for (;j < 8; j++)
+{
+putchar(a[i][j]);
+}
+putchar('\n');
+}
+}
