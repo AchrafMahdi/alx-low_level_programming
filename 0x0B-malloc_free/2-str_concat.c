03a4dfb8c573d609dd85730046031f15e13d8628
Achraf <achraf@example.com>
2024-07-18 02:33:37 +0100

    #2 TESTING

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,8 +10,17 @@ char *str_concat(char *s1, char *s2)
 {
 unsigned int i = 0;
 unsigned int j;
+unsigned length = 0;
 char *combined = NULL;
-combined = malloc(strlen(s1) + strlen(s2) + 1);
+for (j = 0; s1[j] != '\0'; j++)
+{
+length += 1;
+}
+for (j = 0; s2[j] != '\0'; j++)
+{
+length += 1;
+}
+combined = malloc(length + 1);
 if (s1 != NULL)
 {
 for (i = i; s1[i] != '\0'; i++)
