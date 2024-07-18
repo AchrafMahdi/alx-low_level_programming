3a0fb981ac96e0edf8ea08be72dbc8a806100607
Achraf <achraf@example.com>
2024-07-18 01:19:31 +0100

    #0 FIXED

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,14 +7,15 @@
  */
 char *create_array(unsigned int size, char c)
 {
+char *array = malloc(size * sizeof(char));
 if (size == 0){
     return NULL;
 }
-char *array = malloc(size * sizeof(char));
 if (array == NULL){
     return NULL;
 }
-for (unsigned int i = 0; i < size; i++)
+unsigned int i = 0
+for (i; i < size; i++)
 {
     array[i] = c;
 }
