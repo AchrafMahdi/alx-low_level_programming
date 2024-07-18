8619ac5dd02871bce6e313e7961de05999ee3a63
Achraf <achraf@example.com>
2024-07-18 04:28:52 +0100

    #0 DONE

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+/**
+ * main - print argv.
+ * Return: awlays 0.
+ * @argc: argc.
+ * @argv: argv.
+ */
+int main(int argc, char *argv[])
+{    
+printf("%s\n", argv[0]);
+return (0);
+}
