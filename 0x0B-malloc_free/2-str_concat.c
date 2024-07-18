38b3608b2d388021cb6338730ce7e0e682bd42df
Achraf <achraf@example.com>
2024-07-18 02:21:03 +0100

    #2 WILL THIS WORK

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,7 +3,7 @@
  * str_concat - function that concatenates two strings.
  * Return:  pointer or NULL.
  * @s1: string.
- * @s1: string.
+ * @s2: string.
  */
 char *str_concat(char *s1, char *s2)
 {
@@ -20,13 +20,19 @@ for (i = 0; s2[i] != '\0'; i++)
 length += 1;
 }
 combined = malloc(length + 1);
+if (s1 != NULL)
+{
 for (i = 0; s1[i] != '\0'; i++)
 {
 combined[i] = s1[i];
 }
+}
+if (s2 != NULL)
+{
 for (j = 0; s2[j] != '\0'; j++)
 {
 combined[j + i] = s2[i];
+}
 }
 if (combined == NULL)
 {
