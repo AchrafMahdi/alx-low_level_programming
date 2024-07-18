1ceec3db8616a58d7ce2cded8c00ae7ff64ad1b4
Achraf <achraf@example.com>
2024-07-18 01:40:29 +0100

    #1 DONE

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,20 +1,24 @@
 #include <stdlib.h>
 /**
- * create_array - function that returns a pointer to a new space in memory.
+ * _strdup - function that returns a pointer to a new space in memory.
  * Return:  pointer to a new string which is a duplicate of the string str
  * @str: string.
  */
 char *_strdup(char *str)
 {
-char *new_str = malloc(sizeof(char));
-*new_str = str;
+char *new_str = malloc(strlen(str));
+unsigned int i;
 if (str == NULL)
 {
-    return (NULL);
+return (NULL);
 }
-if (*new_str == NULL)
+if (new_str == NULL)
 {
-    return (NULL);
+return (NULL);
+}
+for (i = 0; str[i] != '\0'; i++)
+{
+new_str[i] = str[i];
 }
 return (new_str);
 }
