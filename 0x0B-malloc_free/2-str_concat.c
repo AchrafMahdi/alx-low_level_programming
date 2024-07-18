ab78fc421e66040c47d90251a3d41dcca33f80ab
Achraf <achraf@example.com>
2024-07-18 02:13:23 +0100

    #2 WILL THIS WORK?

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,19 +8,26 @@
 char *str_concat(char *s1, char *s2)
 {
 unsigned int i = 0;
+unsigned int j;
 unsigned int length = 0;
 char *combined = NULL;
 for (i = i; s1[i] != '\0'; i++)
 {
-combined[i] = s1[i];
 length += 1;
 }
 for (i = i; s2[i] != '\0'; i++)
 {
-combined[i] = s2[i];
 length += 1;
 }
 combined = malloc(length + 1);
+for (i = i; s1[i] != '\0'; i++)
+{
+combined[i] = s1[i];
+}
+for (j = 0; s2[j] != '\0'; j++)
+{
+combined[j + i] = s2[i];
+}
 if (combined == NULL)
 {
 return (NULL);
