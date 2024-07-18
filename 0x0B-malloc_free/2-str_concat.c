2776e1b90ecec12d7dc0f92df8389168f40285f7
Achraf <achraf@example.com>
2024-07-18 02:39:45 +0100

    #2 MESSY LOGIC

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,17 +10,19 @@ char *str_concat(char *s1, char *s2)
 {
 unsigned int i = 0;
 unsigned int j;
-unsigned length = 0;
 char *combined = NULL;
-for (j = 0; s1[j] != '\0'; j++)
+if (s1 != NULL && s2 != NULL)
 {
-length += 1;
+combined = malloc(strlen(s1) + strlen(s2) + 1);
 }
-for (j = 0; s2[j] != '\0'; j++)
+if (s1 == NULL && s2 != NULL)
 {
-length += 1;
+combined = malloc(strlen(s2) + 1);
 }
-combined = malloc(length + 1);
+if (s1 != NULL && s2 == NULL)
+{
+combined = malloc(strlen(s1) + 1);
+}
 if (s1 != NULL)
 {
 for (i = i; s1[i] != '\0'; i++)
