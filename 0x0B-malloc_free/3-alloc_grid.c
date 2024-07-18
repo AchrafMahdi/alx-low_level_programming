5c3d349e053773a23ab56ec48ac1f070f731ae93
Achraf <achraf@example.com>
2024-07-18 03:44:38 +0100

    #3 FIXED

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,6 +23,10 @@ return (NULL);
 for (i = 0; i < height; i++)
 {
 grid[i] = malloc(width * sizeof(int));
+if (grid[i] == NULL) {
+free(grid);
+return (NULL);
+}
 for (j = 0; j < width; j++)
 {
 grid[i][j] = 0;
