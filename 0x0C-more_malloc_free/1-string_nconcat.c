29c18814b6b4ebdc893271102b87635082340fad
Achraf <achraf@example.com>
2024-07-19 00:39:56 +0100

    #1 DONE

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * string_nconcat - function that concatenates two strings.
+ * Return:  pointer or NULL.
+ * @s1: string.
+ * @s2: string.
+ * @n: number of bytes.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+unsigned int i = 0;
+unsigned int j;
+char *combined = "";
+if (s1 != NULL && s2 != NULL)
+{
+combined = malloc(strlen(s1) + n + 1);
+}
+if (s1 == NULL && s2 != NULL)
+{
+combined = malloc(strlen(s2) + 1);
+}
+if (s1 != NULL && s2 == NULL)
+{
+combined = malloc(strlen(s1) + 1);
+}
+if (s1 == NULL && s2 == NULL)
+{
+combined = malloc(1);
+}
+if (combined == NULL)
+{
+return (NULL);
+}
+if (s1 != NULL)
+{
+for (i = i; s1[i] != '\0'; i++)
+{
+combined[i] = s1[i];
+}
+}
+if (s2 != NULL && n >= strlen(s2))
+{
+for (j = 0; s2[j] != '\0'; j++)
+{
+combined[j + i] = s2[j];
+}
+}
+return (combined);
+}
