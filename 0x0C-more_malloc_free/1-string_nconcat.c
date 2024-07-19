7c67cd8fa33832a0d77c063c6f5962deb6dd9b9e
Achraf <achraf@example.com>
2024-07-19 01:04:29 +0100

    #1 DONE

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,48 +1,54 @@
 #include <stdlib.h>
 #include <string.h>
+
 /**
- * string_nconcat - function that concatenates two strings.
- * Return:  pointer or NULL.
- * @s1: string.
- * @s2: string.
- * @n: number of bytes.
- */
+* string_nconcat - function that concatenates two strings.
+* Return:  pointer or NULL.
+* @s1: string.
+* @s2: string.
+* @n: number of bytes.
+*/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 unsigned int i = 0;
 unsigned int j;
 char *combined = "";
+
 if (n >= strlen(s2))
 {
 n = strlen(s2);
 }
+
 if (s1 != NULL && s2 != NULL)
 {
 combined = malloc(strlen(s1) + n + 1);
 }
-if (s1 == NULL && s2 != NULL)
+else if (s1 == NULL && s2 != NULL)
 {
-combined = malloc(strlen(s2) + 1);
+combined = malloc(n + 1);
 }
-if (s1 != NULL && s2 == NULL)
+else if (s1 != NULL && s2 == NULL)
 {
 combined = malloc(strlen(s1) + 1);
 }
-if (s1 == NULL && s2 == NULL)
+else
 {
 combined = malloc(1);
 }
+
 if (combined == NULL)
 {
 return (NULL);
 }
+
 if (s1 != NULL)
 {
-for (i = i; s1[i] != '\0'; i++)
+for (; s1[i] != '\0'; i++)
 {
 combined[i] = s1[i];
 }
 }
+
 if (s2 != NULL)
 {
 for (j = 0; j < n; j++)
@@ -50,5 +56,8 @@ for (j = 0; j < n; j++)
 combined[j + i] = s2[j];
 }
 }
+
+combined[i + n] = '\0'; // Add null terminator at the end
+
 return (combined);
 }
