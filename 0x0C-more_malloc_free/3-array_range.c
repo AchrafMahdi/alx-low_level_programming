bb316d607952ff7f55cb6c84930c225bf193bbf0
Achraf <achraf@example.com>
2024-07-19 04:05:29 +0100

    #DONE

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,10 +1,5 @@
 #include <stdlib.h>
-/**
- * *array_range - make  memory for an array
- * @min: int type
- * @max: int type
- * Return:  return pointer to array
- */
+
 int *array_range(int min, int max)
 {
 int *arr;
