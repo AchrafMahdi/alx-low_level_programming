05168ca57cbda3eb8fc5a81a7cb5cf78fd1b8a00
Achraf <achraf@example.com>
2024-07-19 01:24:05 +0100

    #1 FINSIHED

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,26 +12,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 unsigned int i = 0;
 unsigned int j;
 char *combined = "";
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
 if (n > strlen(s2))
 {
 n = strlen(s2);
 }
-if (s1 != NULL && s2 != NULL)
-{
 combined = malloc(strlen(s1) + n + 1);
-}
-if (s1 == NULL && s2 != NULL)
-{
-combined = malloc(strlen(s2) + 1);
-}
-if (s1 != NULL && s2 == NULL)
-{
-combined = malloc(strlen(s1) + 1);
-}
-if (s1 == NULL && s2 == NULL)
-{
-combined = malloc(1);
-}
 if (combined == NULL)
 {
 return (NULL);
