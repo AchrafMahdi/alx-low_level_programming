d42444d6ccbb16cbb31117ed94d7f2384ff59e51
Achraf <achraf@example.com>
2024-07-22 23:19:14 +0100

    #2 idk why checker 4 is red ...

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,39 +1,16 @@
-#include "dog.h"
 #include <stdio.h>
-
+#include "dog.h"
 /**
- * print_dog - Prints a struct dog
- *
- * @d: pointer to d dog
- *
- * Return: nothing
+ * print_dog - function that prints a struct dog.
+ * @d: struct dog.
+ * Return: nothing.
  */
 void print_dog(struct dog *d)
 {
 if (d != NULL)
 {
-
-if (d->name != NULL)
-{
-printf("Name: %s\n",d->name);
-}
-
-else
-{
-printf("Name: (nil)\n");
-}
-
+printf("Name: %s\n",d->name ? d->name : "(nil)");
 printf("Age: %f\n",d->age);
-
-if (d->name != NULL)
-{
-printf("Owner: %s\n",d->owner);
-}
-
-else
-{
-printf("Owner: (nil)\n");
-}
-
+printf("Owner: %s\n",d->owner ? d->owner : "(nil)");
 }
 }
