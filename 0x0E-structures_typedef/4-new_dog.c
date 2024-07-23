4b2db2590a3e9d0fabb55b5852b9dea31923ca1f
Achraf <achraf@example.com>
2024-07-23 02:02:17 +0100

    Dawg

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
- * new_dog - function that creates a new dog. 
+ * new_dog - function that creates a new dog.
  * @name: name of dog.
  * @age: age of dog.
  * @owner: owner of dog.
@@ -15,9 +15,17 @@ if (p == NULL)
 {
 return (NULL);
 }
-if (owner == NULL || name == NULL)
+if (owner == NULL)
 {
+free(name);
 free(p);
+return (NULL);
+}
+if (name == NULL)
+{
+free(owner);
+free(p);
+return (NULL);
 }
 p->name = name;
 p->age = age;
