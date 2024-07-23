b25c098fe1b80cd31f38d66d3417a816df5a91c2
Achraf <achraf@example.com>
2024-07-23 02:17:57 +0100

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,34 +1,88 @@
 #include <stdlib.h>
 #include "dog.h"
+
 /**
- * new_dog - function that creates a new dog.
- * @name: name of dog.
- * @age: age of dog.
- * @owner: owner of dog.
- * Return: pointer or null.
+ * _strlen - Calculate the length of a string
+ * @s: The string
+ * Return: Length of the string
  */
+int _strlen(char *s)
+{
+int length = 0;
+
+while (s[length] != '\0')
+{
+length++;
+}
+
+return (length);
+}
+
+/**
+ * _strcpy - Copy a string to another
+ * @dest: The destination buffer
+ * @src: The source string
+ * Return: Pointer to the destination buffer
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+
+return (dest);
+}
+
+/**
+* new_dog - function that creates a new dog.
+* @name: name of dog.
+* @age: age of dog.
+* @owner: owner of dog.
+* Return: pointer or NULL.
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *p;
+char *name_2, *owner_2;
+int len1;
+int len2;
+
+if (name == NULL || owner == NULL)
+{
+return (NULL);
+}
+
 p = malloc(sizeof(dog_t));
 if (p == NULL)
 {
 return (NULL);
 }
-if (owner == NULL)
+
+len1 = _strlen(name);
+len2 = _strlen(owner);
+
+name_2 = malloc(len1 + 1);
+owner_2 = malloc(len2 + 1);
+
+if (name_2 == NULL || owner_2 == NULL)
 {
-free(name);
+free(name_2);
+free(owner_2);
 free(p);
 return (NULL);
 }
-if (name == NULL)
-{
-free(owner);
-free(p);
-return (NULL);
-}
-p->name = name;
+
+
+_strcpy(owner_2, owner);
+
+p->name = _strcpy(p->name, name);
 p->age = age;
-p->owner = owner;
+p->owner = _strcpy(p->owner, owner);
+
 return (p);
 }
