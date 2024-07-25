a73306b1478918d1b6d818eb486cf5750b0f67c0
Achraf <achraf@example.com>
2024-07-25 01:33:43 +0100

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+/**
+ * print_name - function that prints a name.
+ *
+ * Return: nothing.
+ *
+ * @name: name.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+if (name != NULL)
+{
+f(*name);
+}
+}
