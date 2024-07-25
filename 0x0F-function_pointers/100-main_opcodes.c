5000ddad16fb0d2c4bce4d888f27cc2f9a8cbdd4
Achraf <achraf@example.com>
2024-07-25 05:36:50 +0100

    #1°° PLEASE

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,33 +1,35 @@
-#include "function_pointers.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
- * main - the main function.
- * @argc: the number args.
- * @argv: the array of args.
- *  
+ * main - The main function.
+ * @argc: The number of arguments.
+ * @argv: The array of arguments.
+ * 
  * Return: 0 on success.
  */
-
 int main(int argc, char **argv)
 {
-char *ptr = (char*)main;
-int i;
+    char *ptr = (char *)main;
+    int i;
 
-if (argc != 2)
-{
-printf("Error\n");
-exit(1);
-}
-i = atoi(argv[1]);
-if (i < 0)
-{
-printf("Error\n");
-exit(2);
-}
-while (i--)
-{
-printf("%02hhx%s", *ptr++, i ? " " : "\n");
-}
-return (0);
+    if (argc != 2)
+    {
+        printf("Error\n");
+        exit(1);
+    }
+
+    i = atoi(argv[1]);
+    if (i < 0)
+    {
+        printf("Error\n");
+        exit(2);
+    }
+
+    while (i--)
+    {
+        printf("%02hhx%s", *ptr++, i ? " " : "\n");
+    }
+
+    return (0);
 }
