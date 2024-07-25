2d817777cb3091ef644c3bc9d5cc2265162f6d6c
Achraf <achraf@example.com>
2024-07-25 01:59:55 +0100

    If you spend too much time thinking about a thing, you'll never get it done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,7 +7,6 @@
  * @array: the array that the function will run on each of its elements.
  * @size: the size of the array.
  * @action: the function.
- * 
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
