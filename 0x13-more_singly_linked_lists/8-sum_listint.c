ae0eb1c691c04c5020475d03497fedf3e2105173
Achraf <achraf@example.com>
2024-08-07 02:55:00 +0100

    #8 betty fixed

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -2,7 +2,7 @@
 /**
  * sum_listint -  function that returns the sum of all data of a linked list.
  * Return:  sum of all data or 0.
- * @head: head of linked list. 
+ * @head: head of linked list.
  */
 
 int sum_listint(listint_t *head)
