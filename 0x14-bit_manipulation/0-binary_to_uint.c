24b6b6c0b5ae526002d784c580fd22d6afc9d93a
Achraf <achraf@example.com>
2024-08-10 03:44:01 +0100

    #0 betty fixed

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -14,9 +14,11 @@ if (!b)
 return (0);
 }
 
-for (i = 0; b[i]; i++) {
-if (b[i] != '0' && b[i] != '1') {
-return 0;
+for (i = 0; b[i]; i++)
+{
+if (b[i] != '0' && b[i] != '1')
+{
+return (0);
 }
 num = (num << 1) | (b[i] - '0');
 }
