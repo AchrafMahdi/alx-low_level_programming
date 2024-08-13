e8edcdc5b06e92223a3a0145bf6c7a1acceb78a0
Achraf <achraf@example.com>
2024-08-13 04:11:56 +0100

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
 int create_file(const char *filename, char *text_content)
 {
 int f;
-ssize_t length;
+ssize_t length = 0;
 if (filename == NULL)
 return (-1);
 
@@ -19,7 +19,8 @@ return (-1);
 
 if (text_content != NULL)
 {
-for (length = 0; text_content[length] != '\0'; length++)
+while (text_content[length] != '\0')
+length++;
 if (write(f, text_content, length) != length)
 {
 close(f);
