159326be054e08f005e43df77a7f78cdc784bee8
Achraf <achraf@example.com>
2024-08-13 04:11:02 +0100

    Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * create_file - function that creates a file.
+ * Return: 1 OR -1.
+ * @filename: the file name.
+ * @text_content: data.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+int f;
+ssize_t length;
+if (filename == NULL)
+return (-1);
+
+f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+if (f == -1)
+return (-1);
+
+if (text_content != NULL)
+{
+for (length = 0; text_content[length] != '\0'; length++)
+if (write(f, text_content, length) != length)
+{
+close(f);
+return (-1);
+}
+}
+
+close(f);
+return (1);
+}
